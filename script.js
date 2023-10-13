aa2be9cae8247b774ee0e5648e846a0c4532ae6a
jaydools <134211868+jaydools@users.noreply.github.com>
2023-10-13 14:27:33 -0400

    javascript finished, styling to be applied

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-const apiUrl = 'https://icanhazdadjoke.com/';
+const apiUrl = "https://icanhazdadjoke.com/";
 
-dadContainer.addEventListener("submit", function () {
-    const updateSubmissionsDisplay = document.getElementById("joke-container");
-});
+// dadContainer.addEventListener("submit", function () {
+//     const updateSubmissionsDisplay = document.getElementById("joke-container");  // old event listener for the submit button
+// });
 
-function reload() {
-    window.location.reload();
+// function reload() {
+//     window.location.reload();   // old function for onclick to refresh entire page
+// }
+
+
+function processInput() {
+    const interactInput = document.getElementById("interact").value.toLowerCase();  //convert input to lowercase
+    const jokeContainer = document.getElementById("joke-container");
+    event.preventDefault();                 // prevent page from reloading on submission
+
+
+
+    if (interactInput.includes("joke") || interactInput.includes("jokes")|| interactInput.includes("joke?" || interactInput.includes("jokes?"))) {
+        fetchRandomJoke(jokeContainer);
+    } 
+    else {
+        jokeContainer.innerText = "I think I need another beer..";
+
+    }
 }
-
-
 
 // Function to fetch and display a random joke
 async function fetchRandomJoke() {
 
-  const jokeContainer = document.getElementById('joke-container');
+  const jokeContainer = document.getElementById("joke-container");
 
   try {
     const response = await fetch(apiUrl, {
       headers: {
-        'Accept': 'application/json',      }
+        "Accept": "application/json",      }
     });
 
     if (response.ok) {
       const data = await response.json();
       const joke = data.joke;
 
-      // Display the joke in your HTML
       jokeContainer.innerText = `${joke}`;
     } else {
-      jokeContainer.innerText = 'Failed to fetch a joke.';
+      jokeContainer.innerText = "Failed to fetch a joke.";
     }
   } catch (error) {
     jokeContainer.innerText = `Error: ${error.message}`;
@@ -36,4 +50,15 @@ async function fetchRandomJoke() {
 }
 
 // Call the function to fetch and display a random joke
-fetchRandomJoke();
+// fetchRandomJoke();       //old calling of function
+
+
+// change button to form with placeholder text "hey buddy"
+// accept input as a string
+// if string contains "joke" or "jokes"
+    // print joke
+// else
+    // print "I"m gonna need another beer"
+
+// new onclick function
+
