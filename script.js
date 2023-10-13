9649bf0bb25882b119bf37924cc91d26ba808cec
jaydools <134211868+jaydools@users.noreply.github.com>
2023-10-13 17:48:41 -0400

    final version. Rewrite for more concise code in the future

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const apiUrl = "https://icanhazdadjoke.com/";
 
-// dadContainer.addEventListener("submit", function () {
-//     const updateSubmissionsDisplay = document.getElementById("joke-container");  // old event listener for the submit button
-// });
+function changeHeaderTitle(headerElement) {
+  const headerTitle = headerElement.querySelector('.header__element');
 
-// function reload() {
-//     window.location.reload();   // old function for onclick to refresh entire page
-// }
-
+  if (headerTitle.textContent === "Let's Grill") {
+    headerTitle.textContent = "Let's Roast!!!";
+  } else {
+    headerTitle.textContent = "Let's Grill";
+  }
+}
 
 function processInput() {
-    const interactInput = document.getElementById("interact").value.toLowerCase();  //convert input to lowercase
+    const interactInput = document.getElementById("interact").value.toLowerCase();
     const jokeContainer = document.getElementById("joke-container");
-    event.preventDefault();                 // prevent page from reloading on submission
-
-
+    event.preventDefault();
 
     if (interactInput.includes("joke") || interactInput.includes("jokes")|| interactInput.includes("joke?" || interactInput.includes("jokes?"))) {
         fetchRandomJoke(jokeContainer);
     } 
     else {
         jokeContainer.innerText = "I think I need another beer..";
-
     }
 }
 
-// Function to fetch and display a random joke
 async function fetchRandomJoke() {
 
-  const jokeContainer = document.getElementById("joke-container");
+const jokeContainer = document.getElementById("joke-container");
 
   try {
     const response = await fetch(apiUrl, {
@@ -48,17 +45,3 @@ async function fetchRandomJoke() {
     jokeContainer.innerText = `Error: ${error.message}`;
   }
 }
-
-// Call the function to fetch and display a random joke
-// fetchRandomJoke();       //old calling of function
-
-
-// change button to form with placeholder text "hey buddy"
-// accept input as a string
-// if string contains "joke" or "jokes"
-    // print joke
-// else
-    // print "I"m gonna need another beer"
-
-// new onclick function
-
